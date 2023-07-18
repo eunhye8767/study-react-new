d76c1b6670ec2d4be0de5bdec34f32325012564e
lee.eunhye <lee.eunhye@newswire.co.kr>
2023-07-18 17:48:23 +0900

    useInput() 만들기

---
+++ nomadcoders/hooks-makes/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import Input from './components/Input';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Input />
+    </div>
+  );
+}
+
+export default App;
---
+++ nomadcoders/hooks-makes/src/components/Input.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import { useInput } from "../hooks/useInput";
+
+const Input = () => {
+  const maxLen = (value) => value.length < 10;
+  // @포함되어 있지 않아야 한다.
+  // const checkIncludes = (value) => !value.includes("@");
+
+  const text = useInput("Mr.", maxLen);
+
+  useEffect(() => {
+    console.log(text);
+  }, [text]);
+
+  return (
+    <>
+      <input type="text" placeholder="text..." {...text} />
+    </>
+  );
+};
+
+export default Input;
+
+/**
+ * useInput
+ *
+ *  ㄴ input 관련 hooks 만들어서 활용이 가능.
+ *  ㄴ type: text, search, number, tel, email, radio, checkbox 등
+ */
